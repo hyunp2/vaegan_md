53157970edc0e01ba1446e9ba718aa8bdab36ceb
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-08-26 14:18:13 -0500

    Update vae.py

--- vae.py
+++ vae.py
@@ -103,7 +103,7 @@ class VAE(torch.nn.Module):
         self.encoder = Encoder(hidden_dims=self.hidden_dims_enc, unrolled_dim=self.unrolled_dim)
 #         self.decoder = Decoder(hidden_dims=self.hidden_dims_dec, reference=self.reference, rolled_dim=self.rolled_dim, unrolled_dim=self.unrolled_dim, mha_dimension=self.mha_dimension, nheads=self.nheads, layers=self.layers)
         self.decoder = Decoder(hidden_dims=self.hidden_dims_dec, unrolled_dim=self.unrolled_dim)
-        self.apply(self._init_weights)
+#         self.apply(self._init_weights)
         
     def forward(self, inputs: "Trajectory"):
         x = inputs #Normalized input
